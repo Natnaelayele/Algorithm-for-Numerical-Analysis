068adfd3deb622367ab6257671dd43eed20ae67a
Natnaelayele <nicolasayele@gmail.com>
2023-04-30 10:36:52 +0300

    Change Confusing Variable name to Clarify

--- Bisection Method/Bisection Method.cpp
+++ Bisection Method/Bisection Method.cpp
@@ -12,11 +12,11 @@ double f(double x) {
     return x * x * x - 5 * x + 3;
 }
 //Parameter a the negative root and b the positive root
-//eps parameter is tolerance that given in the question 
+//tolerance parameter is given in the equation that is optional so remove when the tolerance not given
  
-double bisection(double a, double b, double eps) {
+double bisection(double a, double b, double tolerance) {
     double c = (a + b) / 2;
-    while (abs(f(c)) > eps && abs(b - a) > eps) {
+    while (abs(f(c)) > tolerance && abs(b - a) > tolerance) {
         if (f(a) * f(c) < 0) {
             b = c;
         } else {
@@ -29,8 +29,8 @@ double bisection(double a, double b, double eps) {
 }
 
 int main() {
-    double a = 0, b = 1, eps = 0.001;
-    double root = bisection(a, b, eps);
+    double a = 0, b = 1, tolerance = 0.001;
+    double root = bisection(a, b, tolerance);
     cout << "The root of x^3 - 5x + 3 = 0 in the interval [0,1] is: " << root << endl;
     cout<<"Equation Takes :"<<counter<<" Iteration ";
     return 0;
