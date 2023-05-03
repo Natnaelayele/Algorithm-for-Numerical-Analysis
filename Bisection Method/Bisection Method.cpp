bed25347fe68d5c04e983e9bc536eaa421642825
Natnaelayele <nicolasayele@gmail.com>
2023-05-03 10:21:37 +0300

    code comments to read.md

--- Bisection Method/Bisection Method.cpp
+++ Bisection Method/Bisection Method.cpp
@@ -1,19 +1,11 @@
-//Bisection Method
 #include <iostream>
 #include <cmath>
 using namespace std;
-int counter=0;   //Here is Counter Global Variable for counting the iteration of the program that excute
-
-// Equation f(x)=x^3-5x+3  
-
-//first you should find root interval  for equation 
+int counter=0;  
 
 double f(double x) {
     return x * x * x - 5 * x + 3;
 }
-//Parameter a the negative root and b the positive root
-//tolerance parameter is given in the equation that is optional so remove when the tolerance not given
- 
 double bisection(double a, double b, double tolerance) {
     double c = (a + b) / 2;
     while (abs(f(c)) > tolerance && abs(b - a) > tolerance) {
